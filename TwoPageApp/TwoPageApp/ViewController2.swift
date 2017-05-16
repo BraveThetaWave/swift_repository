29035b7e8bd9c5e90eb7083a780f5f12eb222ee0
BraveThetaWave <sam@my.prideprepschool.org>
2017-05-16 13:15:15 -0700

    two page app

---
+++ TwoPageApp/TwoPageApp/ViewController2.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController2.swift
+//  TwoPageApp
+//
+//  Created by Student on 5/16/17.
+//  Copyright © 2017 Sam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    @IBOutlet weak var MoneyLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
